479ac14d3527bb2a9d02f2894d472bbeff211849
Tim Schindler <xh3b4sd@gmail.com>
2020-10-20 23:45:08 +0200

    create kubernetes infrastructure in eks

--- cmd/create/eks/command.go
+++ cmd/create/eks/command.go
@@ -9,7 +9,26 @@ import (
 const (
 	name  = "eks"
 	short = "Create kubernetes infrastructure environments for eks."
-	long  = "Create kubernetes infrastructure environments for eks."
+	long  = `Create kubernetes infrastructure environments for eks. The basis for this
+type of environment is a remote eks cluster. EKS stands for elastic
+kubernetes service. For more information check the eks website.
+
+    https://aws.amazon.com/eks
+
+In order to create and setup the kind cluster we need to properly configure
+the kia command line tool. This is done via its config file, tracked on the
+local file system. The kia base path must be set. This is the local path of
+the kia repository from which general templates are read. Add the following
+line to your config file according to your local setup.
+
+    kia: "~/project/xh3b4sd/kia/"
+
+The sec base path must be set. This is the local path of the sec repository
+from which secret data is read. Add the following line to your config file
+according to your local setup.
+
+    sec: "~/project/xh3b4sd/sec/"
+`
 )
 
 type Config struct {
--- cmd/create/eks/flag.go
+++ cmd/create/eks/flag.go
@@ -1,30 +1,32 @@
 package eks
 
 import (
+	"os"
+
 	"github.com/spf13/cobra"
 	"github.com/xh3b4sd/tracer"
+
+	"github.com/xh3b4sd/kia/pkg/config"
+	"github.com/xh3b4sd/kia/pkg/env"
 )
 
 type flag struct {
-	Github struct {
-		Organization string
-		Repository   string
-	}
+	Kia string
+	Sec string
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
-	cmd.Flags().StringVarP(&f.Github.Organization, "github-organization", "o", "", "Github organization to generate code for.")
-	cmd.Flags().StringVarP(&f.Github.Repository, "github-repository", "r", "", "Github repository to generate code for.")
+	cmd.Flags().StringVarP(&f.Kia, "kia", "k", config.GetKia(os.Getenv(env.KiaBasePath)), "Kia base path on the local file system.")
+	cmd.Flags().StringVarP(&f.Sec, "sec", "s", config.GetSec(os.Getenv(env.SecBasePath)), "Sec base path on the local file system.")
 }
 
 func (f *flag) Validate() error {
-	{
-		if f.Github.Organization == "" {
-			return tracer.Maskf(invalidFlagError, "-o/--github-organization must not be empty")
-		}
-		if f.Github.Repository == "" {
-			return tracer.Maskf(invalidFlagError, "-r/--github-repository must not be empty")
-		}
+	if f.Kia == "" {
+		return tracer.Maskf(invalidFlagError, "-k/--kia must not be empty")
+	}
+
+	if f.Sec == "" {
+		return tracer.Maskf(invalidFlagError, "-s/--sec must not be empty")
 	}
 
 	return nil
